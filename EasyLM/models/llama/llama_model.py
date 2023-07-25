1c0de6fa22fcd642496d1d62a8f52db2ea791023
young-geng <young.gengxy@gmail.com>
2023-07-25 04:26:04 -0700

    Support for memory efficient (scanned) attention and scanned mlp

--- EasyLM/models/llama/llama_model.py
+++ EasyLM/models/llama/llama_model.py
@@ -16,6 +16,7 @@ from flax.linen import combine_masks, make_causal_mask
 from flax.linen.attention import dot_product_attention_weights
 from flax.traverse_util import flatten_dict, unflatten_dict
 from flax.linen import partitioning as nn_partitioning
+import einops
 
 import sentencepiece as spm
 from transformers.configuration_utils import PretrainedConfig
@@ -29,6 +30,7 @@ from ml_collections import ConfigDict
 from ml_collections.config_dict import config_dict
 from mlxu import function_args_to_config, load_pickle, open_file
 
+from EasyLM.memory_efficient_attention import dot_product_attention_multihead as efficient_dot_product_attention
 from EasyLM.jax_utils import (
     with_sharding_constraint, get_jax_mesh, get_gradient_checkpoint_policy
 )
@@ -187,6 +189,11 @@ class LLaMAConfig(PretrainedConfig):
         remat_block='nothing_saveable',
         remat_attention='',
         remat_mlp='',
+        scan_attention=False,
+        scan_mlp=False,
+        scan_query_chunk_size=1024,
+        scan_key_chunk_size=2048,
+        scan_mlp_chunk_size=1024,
         fcm_min_ratio=0.0,
         fcm_max_ratio=0.0,
         **kwargs,
@@ -206,6 +213,11 @@ class LLaMAConfig(PretrainedConfig):
         self.remat_block = remat_block
         self.remat_attention = remat_attention
         self.remat_mlp = remat_mlp
+        self.scan_attention = scan_attention
+        self.scan_mlp = scan_mlp
+        self.scan_query_chunk_size = scan_query_chunk_size
+        self.scan_key_chunk_size = scan_key_chunk_size
+        self.scan_mlp_chunk_size = scan_mlp_chunk_size
         self.fcm_min_ratio = fcm_min_ratio
         self.fcm_max_ratio = fcm_max_ratio
         super().__init__(
@@ -514,19 +526,40 @@ class FlaxLLaMAAttention(nn.Module):
         )
 
         # usual dot product attention
-        attn_weights = dot_product_attention_weights(
-            xq,
-            xk,
-            bias=attention_bias,
-            dropout_rng=dropout_rng,
-            dropout_rate=self.config.attn_pdrop,
-            deterministic=deterministic,
-            dtype=jnp.promote_types(self.dtype, jnp.float32),
-            precision=self.precision,
-        )
-        attn_weights = with_sharding_constraint(attn_weights, PS(("dp", "fsdp"), "mp", None, None))
+        if self.config.scan_attention:
+            attn_weights = None
+            attention_mask = einops.rearrange(
+                combine_masks(attention_mask, fcm_mask),
+                '... s q k -> ... s 1 q k'
+            )
+            attn_output = efficient_dot_product_attention(
+                xq,
+                xk,
+                xv,
+                bias=attention_mask,
+                dropout_rng=dropout_rng,
+                dropout_rate=self.config.attn_pdrop,
+                enable_dropout=not deterministic and self.config.attn_pdrop > 0.0,
+                float32_logits=True,
+                causal_mask=True,
+                precision=self.precision,
+                query_chunk_size=self.config.scan_query_chunk_size,
+                key_chunk_size=self.config.scan_key_chunk_size,
+            )
+        else:
+            attn_weights = dot_product_attention_weights(
+                xq,
+                xk,
+                bias=attention_bias,
+                dropout_rng=dropout_rng,
+                dropout_rate=self.config.attn_pdrop,
+                deterministic=deterministic,
+                dtype=jnp.promote_types(self.dtype, jnp.float32),
+                precision=self.precision,
+            )
+            attn_weights = with_sharding_constraint(attn_weights, PS(("dp", "fsdp"), "mp", None, None))
+            attn_output = jnp.einsum("...hqk,...khd->...qhd", attn_weights, xv, precision=self.precision)
 
-        attn_output = jnp.einsum("...hqk,...khd->...qhd", attn_weights, xv, precision=self.precision)
         attn_output = self._merge_heads(attn_output)
         attn_output = self.wo(attn_output)
         attn_output = self.resid_dropout(attn_output, deterministic=deterministic)
@@ -642,10 +675,37 @@ class FlaxLLaMABlock(nn.Module):
         attn_output = attn_outputs[0]
         hidden_states = hidden_states + attn_output
 
-        feed_forward_hidden_states = self.feed_forward(
-            self.ffn_norm(hidden_states),
-            deterministic,
-        )
+        feed_forward_input = self.ffn_norm(hidden_states)
+
+        if self.config.scan_mlp:
+            feed_forward_input = einops.rearrange(
+                feed_forward_input,
+                '... (b s) d -> ... b s d',
+                b=self.config.scan_mlp_chunk_size
+            )
+
+            def mlp_forward(mlp, carry, x):
+                return None, mlp(x, deterministic)
+
+            scan_axis = feed_forward_input.ndim - 3
+
+            _, feed_forward_hidden_states = nn.scan(
+                mlp_forward,
+                variable_broadcast="params",
+                split_rngs={"params": False, "dropout": True},
+                in_axes=scan_axis,
+                out_axes=scan_axis,
+            )(self.feed_forward, None, feed_forward_input)
+            feed_forward_hidden_states = einops.rearrange(
+                feed_forward_hidden_states,
+                '... b s d -> ... (b s) d'
+            )
+        else:
+            feed_forward_hidden_states = self.feed_forward(
+                feed_forward_input,
+                deterministic,
+            )
+
         hidden_states = hidden_states + feed_forward_hidden_states
 
         return (hidden_states,) + attn_outputs[1:]
@@ -845,7 +905,7 @@ class FlaxLLaMABlockCollection(nn.Module):
             )
             fcm_mask = jax.random.uniform(
                 self.make_rng('fcm'),
-                shape=(batch_size, 1, seq_length, seq_length)
+                shape=(batch_size, 1, 1, seq_length)
             ) > fcm_ratio
             fcm_mask = fcm_mask.at[:, :, :, 0].set(True)
             fcm_mask = fcm_mask.astype('bool')
