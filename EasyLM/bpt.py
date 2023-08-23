72eb6cd7da4aacbbdde507327713dae15ff7a982
haoliu <lhao499@gmail.com>
2023-08-23 04:12:57 +0000

    added doc and tests to BPT and simplified the code

--- EasyLM/bpt.py
+++ EasyLM/bpt.py
@@ -1,19 +1,25 @@
-from einops import rearrange
-import jax
-import jax.numpy as jnp
-import functools
-import jax.lax as lax
-from typing import NamedTuple
-from flax.linen import partitioning as nn_partitioning
-import flax.linen as nn
+"""An implementation of Blockwise parallel transformer https://arxiv.org/abs/2305.19370
 
+Also include a reference implementation of memory-efficient transformer https://arxiv.org/abs/2112.05682
+"""
+
+import functools
+from typing import NamedTuple
+
+import flax.linen as nn
+import jax
+import jax.lax as lax
+import jax.numpy as jnp
+from einops import rearrange
 
 '''
 Computing ffn blockwise without materializing the large hidden tensor, training 4x longer sequences than the memory-efficient transformer.
 Blockwise parallel transformer https://arxiv.org/abs/2305.19370 Liu et al. 2023
 '''
 def blockwise_ffn(remat_ffn, inputs, chunk_size, deterministic):
-    # remat_ffn: a rematerialized ffn
+    # remat_ffn: a rematerialized ffn with policy jax.checkpoint_policies.nothing_saveable()
+    # inputs: (batch, seq_len, dim)
+    # chunk_size: the chunk size to split the sequence
     inputs = rearrange(inputs, 'b (c n) d -> b c n d', c=chunk_size)
     def scan_ffn(remat_ffn, carry, hidden_states):
         outputs = remat_ffn(hidden_states, deterministic=deterministic)
@@ -39,19 +45,32 @@ def blockwise_attn(query, key, value, bias, deterministic,
         dropout_rng, attn_pdrop, causal, query_chunk_size,
         key_chunk_size, dtype, policy, precision, float32_logits,
         prevent_cse):
+    # query, key, value: (batch, seq_len, num_heads, dim_per_head)
+    # bias: (batch, seq_len) can be used to mask out attention (e.g. padding)
+    # causal: whether to use causal mask
+    # policy: one of jax.checkpoint_policies
     query = query / jnp.sqrt(query.shape[-1]).astype(dtype)
-    q_len = query.shape[1]
-    kv_len = key.shape[1]
     if float32_logits:
         query = query.astype(jnp.float32)
         key = key.astype(jnp.float32)
-    query = rearrange(query, 'b (c n) h d -> n b c h d', c=query_chunk_size)
-    key, value = map(lambda t: rearrange(t, 'b (c n) h d -> n b c h d', c=key_chunk_size), (key, value))
-    num_q, batch, _, num_heads, dim_per_head = query.shape
-    num_kv = key.shape[0]
 
-    for bias_dim, broadcast_dim in zip(bias.shape, (batch, num_heads, q_len, kv_len)):
-        assert bias_dim == 1 or bias_dim == broadcast_dim
+    batch, q_len, num_heads, dim_per_head = query.shape
+    batch, kv_len, num_heads, dim_per_head = key.shape
+    batch, kv_len, num_heads, dim_per_head = value.shape
+
+    num_q = q_len // query_chunk_size
+    num_kv = kv_len // key_chunk_size
+    query = query.reshape((batch, num_q, query_chunk_size, num_heads, dim_per_head))
+    key = key.reshape((batch, num_kv, key_chunk_size, num_heads, dim_per_head))
+    value = value.reshape((batch, num_kv, key_chunk_size, num_heads, dim_per_head))
+
+    query = jnp.moveaxis(query, 1, 0)
+    key = jnp.moveaxis(key, 1, 0)
+    value = jnp.moveaxis(value, 1, 0)
+
+    if bias is not None:
+        for bias_dim, broadcast_dim in zip(bias.shape, (batch, num_heads, q_len, kv_len)):
+            assert bias_dim == 1 or bias_dim == broadcast_dim
     if not deterministic and attn_pdrop > 0.0:
         attn_dropout_rng, dropout_rng = jax.random.split(dropout_rng)
         attn_dropout = jax.random.bernoulli(attn_dropout_rng, attn_pdrop, (batch, num_heads, q_len, kv_len))
@@ -63,11 +82,11 @@ def blockwise_attn(query, key, value, bias, deterministic,
         query_chunk_size, key_chunk_size, bias, deterministic,
         attn_dropout, attn_pdrop, causal, dtype)
 
-    def _query_chunk_attention(args):
+    def scan_attention(args):
         query_chunk, query_chunk_idx = args
 
         @functools.partial(jax.checkpoint, prevent_cse=prevent_cse, policy=policy)
-        def summarize_chunk(carry, args):
+        def scan_kv_block(carry, args):
             key_chunk, value_chunk, key_chunk_idx = args
             (numerator, denominator, prev_max_score) = carry
             attn_weights = jnp.einsum('bqhd,bkhd->bqhk', query_chunk, key_chunk, precision=precision)
@@ -80,7 +99,7 @@ def blockwise_attn(query, key, value, bias, deterministic,
             max_score = jax.lax.stop_gradient(max_score)
             exp_weights = jnp.exp(attn_weights - max_score)
             exp_values = jnp.einsum(
-                'bqhv,bvhf->bqhf', exp_weights, value_chunk, precision=precision
+                'bqhv,bvhd->bqhd', exp_weights, value_chunk, precision=precision
             )
             correction = jnp.exp(prev_max_score - max_score)
             numerator = numerator * correction + exp_values
@@ -95,7 +114,7 @@ def blockwise_attn(query, key, value, bias, deterministic,
             return jax.lax.cond(
                 skip_block,
                 lambda carry, args: (carry, None),
-                summarize_chunk,
+                scan_kv_block,
                 carry,
                 args,
             )
@@ -112,7 +131,7 @@ def blockwise_attn(query, key, value, bias, deterministic,
         return outputs
 
     _, res = lax.scan(
-        lambda _, x: ((), _query_chunk_attention(x)),
+        lambda _, x: ((), scan_attention(x)),
         (), xs=(query, jnp.arange(0, num_q))
     )
     res = rearrange(res, 'n b c h d -> b (n c) h d')
@@ -158,3 +177,37 @@ def _chunk_attention_bias(query_chunk_size, key_chunk_size,
         )
         chunk_bias += attn_dropout_slice * jnp.finfo(dtype).min
     return chunk_bias.astype(dtype)
+
+
+if __name__ == '__main__':
+    # test
+    def reference_attn(query, key, value, causal, dtype):
+        query = query / jnp.sqrt(query.shape[-1]).astype(dtype)
+        logits = jnp.einsum("bqhc,bkhc->bhqk", query, key)
+        if causal:
+            mask_value = jnp.finfo(logits.dtype).min
+            _, q_seq_len, _, _ = query.shape
+            _, kv_seq_len, _, _ = key.shape
+            mask_shape = (q_seq_len, kv_seq_len)
+            row_ids = jax.lax.broadcasted_iota(jnp.int32, mask_shape, 0)
+            col_ids = jax.lax.broadcasted_iota(jnp.int32, mask_shape, 1)
+            causal_mask = (row_ids < col_ids)[None, None, :, :]
+            logits = logits + jnp.where(causal_mask, mask_value, 0.0)
+        weights = jax.nn.softmax(logits, axis=-1)
+        out = jnp.einsum("bhqk,bkhc->bqhc", weights, value)
+        return out
+
+    # random inputs
+    shape = (1, 32, 8, 64)
+    query = jax.random.normal(jax.random.PRNGKey(0), shape)
+    key = jax.random.normal(jax.random.PRNGKey(1), shape)
+    value = jax.random.normal(jax.random.PRNGKey(2), shape)
+
+    causal = True
+    chunk_size = 4
+    policy = jax.checkpoint_policies.nothing_saveable()
+
+    blockwise = blockwise_attn(query, key, value, None, False, None, 0.0, causal, chunk_size, chunk_size, jnp.float32, policy, 'float32', True, False)
+    reference = reference_attn(query, key, value, causal, 'float32')
+
+    assert jnp.allclose(reference, blockwise, atol=1e-6)
